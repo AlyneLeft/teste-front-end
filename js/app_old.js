a82f1d6d1768047cbb6e4cc77c3c72a614466898
AlyneLeft <alyne_vasconcellos@hotmail.com>
2017-01-26 02:14:29 -0200

    novo app.js/nova logica - otimizacao da busca

--- js/app_old.js
+++ js/app_old.js
@@ -16,16 +16,12 @@ function appController($scope) {
   $scope.show_details = false;
   $scope.show_nav = false;
   $scope.show_movie_list = true;
-  $scope.items_per_search;
-  $scope.selected = 0;
 
   $scope.searchFilm = function(){
       var url_search = 'http://www.omdbapi.com/?s=' + $scope.search_field;
-      /*confere se a busca atual não é exatamente igual à anterior, evitando buscas desnecessárias*/
       if($scope.last_search != $scope.search_field){
         $.get(url_search, function(response) {
           if(response.Response == "True"){
-          	$scope.items_per_search = response.Search.length;
             var qty_pages = Math.ceil(response.totalResults / response.Search.length);
             $scope.resp = response;
             $scope.pages = [];
@@ -38,12 +34,10 @@ function appController($scope) {
             $scope.show_details = false;
             $scope.obj_details = {};
 
-            for(var i = 2; i <= $scope.items_nav_per_page; i++){
+            for(var i = 2; i <= qty_pages; i++){
               $.get(url_search+"&page="+i, function(response){
-              	if(response.Response == "True"){
-	                for(var j = 0; j < response.Search.length; j++){
-	                  $scope.resp.Search.push(response.Search[j]);
-	                }
+                for(var j = 0; j < response.Search.length; j++){
+                  $scope.resp.Search.push(response.Search[j]);
                 }
               });
             }
@@ -86,51 +80,17 @@ function appController($scope) {
   };
 
   $scope.changePage = function(numSelected){
-  	$scope.selected = numSelected.num - 1;
     $scope.index_max = (numSelected.num * $scope.items_per_page) - 1;
     $scope.index_min = ($scope.index_max - $scope.items_per_page) + 1;
   };
 
   $scope.nextPage = function(){
     var current_page = Math.ceil($scope.index_max / $scope.items_per_page);
-
     if(current_page < $scope.qty_pages_final){
-
-    	/*confere se o número da navegação era o último, confirmando a necessidade de apresentar números novos na aba de navegação*/
       if(current_page % $scope.items_nav_per_page == 0){
-
-      	/*altera o index mínimo e máximo p/ indicar quais filmes devem ficar visíveis na tela*/
         $scope.index_nav_min += $scope.items_nav_per_page;
         $scope.index_nav_max += $scope.items_nav_per_page;
-
         $scope.changePage({num: current_page + 1});
-
-        /*confere se a quantidade de filmes no meu objeto já atingiu o total de filmes econtrados na busca*/
-        if($scope.resp.Search.length < $scope.resp.totalResults){
-        	console.log("qtd de itens no meu objeto = ", $scope.resp.Search.length);
-        	console.log("qtd de itens encontrados na busca = ", $scope.resp.totalResults);
-        	var qty;
-
-        	/*confere se a quantidade de filmes que falta buscar na api é maior que a quantidade que estamos tentando buscar (40 por chamada)*/
-        	if(($scope.resp.totalResults - $scope.resp.Search.length) > ($scope.items_nav_per_page * $scope.items_per_search)){
-        		qty = $scope.items_nav_per_page;
-        		console.log("entrei no if, portanto qty = ", qty);
-        	}else{
-        		/*verifica quantas buscas ainda são necessárias p/ alcançar a quantidade total de filmes encontrados*/
-        		qty = Math.ceil(($scope.resp.totalResults - $scope.resp.Search.length) / $scope.items_per_search);
-        		console.log("entrei no else, portanto qty = ", qty);
-        	}
-        	for(var i = 1; i <= qty; i++){
-      			var url_search = 'http://www.omdbapi.com/?s=' + $scope.last_search + "&page=" + (current_page + i);
-      			console.log("url_search: ", url_search);
-      			$.get(url_search, function(response){
-              for(var j = 0; j < response.Search.length; j++){
-                $scope.resp.Search.push(response.Search[j]);
-              }
-              $scope.$apply();
-            });
-      		}
-        }
       }else{
         $scope.changePage({num: current_page + 1});
       }
